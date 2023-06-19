594ec3545eb17699ef10444b22e0f8eb8498f494
Alex Domsa <alex.domsa@lola.tech>
2023-06-19 11:18:40 +0300

    Add authentication

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-using Microsoft.EntityFrameworkCore;
 using UrlShortner.Data;
 using UrlShortner.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UrlShortner.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,11 @@ builder.Services.AddRazorPages();
 
 var connectionString = builder.Configuration.GetConnectionString("UrlShortnerConnection");
 builder.Services.AddDbContext<UrlShortnerContext>(options => options.UseSqlServer(connectionString));
+
+var authConnectionString = builder.Configuration.GetConnectionString("UrlShortnerIdentityDbContextConnection");
+builder.Services.AddDbContext<UrlShortnerIdentityDbContext>(options => options.UseSqlServer(authConnectionString)); 
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<UrlShortnerIdentityDbContext>();
 
 builder.Services.AddScoped<UrlService>();
 
@@ -26,7 +33,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
